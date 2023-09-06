86b359c9af325b5ca48c1c6135581aaa7a109409
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-06 15:31:39 +0200

    Access page and retrieve array of images

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import puppeteer from 'puppeteer';
 
 const folderName = './memes';
 try {
@@ -6,5 +7,29 @@ try {
     fs.mkdirSync(folderName);
   }
 } catch (err) {
-  console.error(err);
+  console.error('directory already exists');
 }
+
+(async () => {
+  // Launch the browser
+  const browser = await puppeteer.launch();
+
+  // Create a page
+  const page = await browser.newPage();
+
+  // Go to your site
+  await page.goto('https://memegen-link-examples-upleveled.netlify.app/');
+
+  // Query for an element handle.
+  //  const element = await page.waitForSelector('div > .class-name');
+  const images = await page.evaluate(() =>
+    Array.from(document.images, (e) => e.src),
+  );
+
+  console.log(images);
+  // Dispose of handle
+  // await element.dispose();
+
+  // Close browser.
+  await browser.close();
+})();

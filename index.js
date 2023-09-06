2a007fbcc0777e86b0d59d96efe7214cdbcdc9f0
Clara Mrkos <mrkos.clara@gmail.com>
2023-09-06 14:43:59 +0200

    Create directory function

---
+++ index.js
@@ -0,0 +1,10 @@
+import * as fs from 'fs';
+
+const folderName = './memes';
+try {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName);
+  }
+} catch (err) {
+  console.error(err);
+}
